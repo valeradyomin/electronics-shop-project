30ebbb1f107de0386b8abbb826ae32ae9bf1c819
Valera Demin <wahrwolffe@gmail.com>
2023-09-12 23:54:16 +0300

    main task done

--- src/item.py
+++ src/item.py
@@ -13,7 +13,14 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+        Item.all.append(self)
+
+    def __repr__(self):
+        return f"Item('name={self.name}', 'price={self.price}', 'quantity={self.quantity}')"
 
     def calculate_total_price(self) -> float:
         """
@@ -21,10 +28,11 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        pass
+        total = self.price * self.quantity
+        return total
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        self.price = self.price * Item.pay_rate
