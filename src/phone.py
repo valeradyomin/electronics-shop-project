685108176b0ef9bcfa90a49b6c2a7e5604e646c7
Valera Demin <wahrwolffe@gmail.com>
2023-09-26 01:58:50 +0300

    HW4: new module add class Phone

---
+++ src/phone.py
@@ -0,0 +1,23 @@
+from src.item import Item
+
+
+class Phone(Item):
+    def __init__(self, name: str, price: float, quantity: int, number_of_sim: int) -> None:
+        super().__init__(name, price, quantity)
+        self.__number_of_sim = number_of_sim
+
+    def __repr__(self):
+        return f"{__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
+
+    @property
+    def number_of_sim(self):
+        return self.__number_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, number):
+        if isinstance(number, int) and number > 0:
+            self.__number_of_sim = number
+        else:
+            raise ValueError("Количество физических SIM-карт должно быть целым числом больше нуля.")
+
+
