fd099395235f38fed3f3df7c4d360986e10a305b
Valera Demin <wahrwolffe@gmail.com>
2023-09-28 17:45:39 +0300

    HW5: keyboard/mixin class ready

---
+++ src/keyboard.py
@@ -0,0 +1,21 @@
+from src.item import Item
+
+
+class MixinKeyboardLang:
+    def __init__(self, name, price, quantity):
+        super().__init__(name, price, quantity)
+        self.__language = "EN"
+
+    @property
+    def language(self):
+        return self.__language
+
+    def change_lang(self):
+        if self.__language == "EN":
+            self.__language = "RU"
+        else:
+            self.__language = "EN"
+
+
+class Keyboard(MixinKeyboardLang, Item):
+    pass
